5629ac5a1c80869a115dbc59ca8e5fb6fb5b338e
Sumedh Shende <sumedh.shende@hotmail.com>
2019-04-24 11:43:01 -0700

    Card Shuffle

---
+++ cards.cpp
@@ -0,0 +1,41 @@
+#include "../DataStructures/include/common.h"
+#include "../DataStructures/include/utils.h"
+
+void swap( int& a, int& b) {
+	int t = a;
+	a = b;
+	b = t;
+}  
+/*void printv(vector<int>& vA) {
+        for(int i=0; i<vA.size(); i++) 
+                cout << vA[i] << " ";
+        cout << endl;
+}*/
+void getNewDeck(vector<int>& cards) {
+	for(int i=0; i<52; i++ ) {
+		cards.push_back(i%13 + 1);
+	}
+	printv(cards);
+}
+
+void shuffle(vector<int>& cards) {
+	srand(time(NULL));
+	int arr[13] ={0};
+	for(int i=0; i<cards.size();i++){
+		int ai = rand() % (cards.size() - i);
+		arr[cards[ai]-1]++;
+		//cout << ai << ", " << cards[ai] << " --> " << endl;	
+		swap(cards[ai], cards[cards.size()- (i+1)]);	
+	}
+	for(int i=0; i<13;i++){
+		cout << i+1 << ", " << arr[i] << endl;
+	}
+	printv(cards);
+} 
+
+int main() {
+	vector<int> cards;
+	getNewDeck(cards);
+	shuffle(cards);	
+	return 0;
+}
